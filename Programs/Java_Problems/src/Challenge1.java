5faf3b94e4a47b0509b354b519f862938e6bb9cc
KM-NISHA <kmnisha979@gmail.com>
2024-02-27 12:30:36 +0530

    java first problm

---
+++ Programs/Java_Problems/src/Challenge1.java
@@ -0,0 +1,8 @@
+
+public class Challenge1 {
+  public static void main(String[] args) {
+    System.out.println("*\n* * \n* * * \n* * * * \n* * * * *\n* * * * * *");
+    System.out.println("* * * * *\n* * * * \n* * *\n* * \n*");
+  }
+
+}
